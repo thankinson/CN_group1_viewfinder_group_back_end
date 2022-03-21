a2ed5ec1d80c55cbf4026e63ab1af6069c1c3b73
Alex Wayland <89842550+web-lynx@users.noreply.github.com>
2022-03-21 16:34:17 +0000

    Added the basic user schema with username, email, password, and watchlist.

--- src/user/userModel.js
+++ src/user/userModel.js
@@ -1,1 +1,12 @@
-// user model
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    watchlist: [String],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
